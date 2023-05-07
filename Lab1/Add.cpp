faecf8aac746a3740f275f43e13b28ae853a572a
Wonjae <yww24@naver.com>
2023-05-07 12:41:39 +0900

    C++ 첫 주 실습

---
+++ Lab1/Add.cpp
@@ -0,0 +1,9 @@
+#include "Add.h"
+
+namespace lab1
+{
+	int Add(int a, int b)
+	{
+		return a + b;
+	}
+}
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+#include "Add.h"
+
+
 int main()
 {
+	using namespace std;
 
+	int a = 3;
+	int b = 5;
+
+	cout << lab1::Add(a, b); //int Add() in Add.cpp
+
+	return 0;
 }

5fa040cdfb091b0888dbcd3d30aab54b46b53dd9
Wonjae <yww24@naver.com>
2023-05-13 14:21:44 +0900

    음수예외처리 & ctr+k+d

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -4,70 +4,71 @@
 
 namespace lab2
 {
-    void PrintIntegers(std::istream& in, std::ostream& out)
-    {
-        using namespace std;
-        int number;
-        string trash;
+	using namespace std;
+
+	void PrintIntegers(std::istream& in, std::ostream& out)
+	{
+		int number;
+		string trash;
 
 
-        out << setw(12) << right << "oct"
-            << setw(11) << "dec"
-            << setw(9)  << "hex" << "\n";
+		out << setw(12) << right << "oct"
+			<< setw(11) << "dec"
+			<< setw(9) << "hex" << "\n";
 
-        out << setw(13) << setfill('-') << right << ' '
-            << setw(11) << ' '
-            << setw(9)  << "\n";
+		out << setw(13) << setfill('-') << right << ' '
+			<< setw(11) << ' '
+			<< setw(9) << "\n";
 
 
-        while (!in.eof()) 
-        {
-            in >> number;
+		while (!in.eof())
+		{
+			in >> number;
 
-            if (in.fail()) 
-            {
-                in.clear();
-                in >> trash;
-            }
-            else 
-            {
-                out << setfill(' ') << uppercase
-                    << oct << setw(12) << right << number;
-                out << dec << setw(11) << number;
-                out << hex << setw(9)  << number << "\n";
-            }
-        }
-    }
+			if (in.fail())
+			{
+				in.clear();
+				in >> trash;
+			}
+			else
+			{
+				out << setfill(' ') << uppercase
+					<< oct << setw(12) << right << number;
+				out << dec << setw(11) << number;
+				out << hex << setw(9) << number << "\n";
+			}
+		}
+	}
 
-    void PrintMaxFloat(std::istream& in, std::ostream& out)
-    {
-        using namespace std;
+	void PrintMaxFloat(std::istream& in, std::ostream& out)
+	{
+		using namespace std;
 
-        float number;
-        float max = FLT_MIN;
-        string trash;
+		float number;
+		float max = -FLT_MAX;
+		string trash;
 
-        while (!in.eof())
-        {
-            in >> number;
+		while (!in.eof())
+		{
+			in >> number;
 
-            if (in.fail())
-            {
-                in.clear();
-                in >> trash;
-            }
-            else
-            {
-                out << setw(5) << ' ' << setw(15) << internal 
-                    << fixed << showpos << setprecision(3) << number << "\n";
+			if (in.fail())
+			{
+				in.clear();
+				in >> trash;
+			}
+			else
+			{
+				out << setw(5) << ' ' << setw(15) << internal
+					<< fixed << showpos << setprecision(3) << number << "\n";
 
-                if (max < number)
-                {
-                    max = number;
-                }
-            }
-        }
-        out << setw(5) << left << "max:" << setw(15) << internal
-            << fixed << showpos << setprecision(3) << max << "\n";
-    }
+				if (max < number)
+				{
+					max = number;
+				}
+			}
+		}
+		out << setw(5) << left << "max:" << setw(15) << internal
+			<< fixed << showpos << setprecision(3) << max << "\n";
+	}
 }

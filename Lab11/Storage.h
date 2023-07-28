111bd24425d797f8819093454a6f7a19c0baf038
Wonjae <yww24@naver.com>
2023-07-29 03:47:24 +0900

    실습11

--- Lab11/Storage.h
+++ Lab11/Storage.h
@@ -10,38 +10,84 @@ namespace lab11
 	public:
 		Storage(unsigned int length);
 		Storage(unsigned int length, const T& initialValue);
+		Storage(Storage&& other);
+
+		Storage<T> operator=(Storage&& other);
 
 		bool Update(unsigned int index, const T& data);
 		const std::unique_ptr<T[]>& GetData() const;
 		unsigned int GetSize() const;
+	private:
+		std::unique_ptr<T[]> mArray;
+		unsigned int mLength;
 	};
 
 	template<typename T>
 	Storage<T>::Storage(unsigned int length)
+		: mLength(length)
+		, mArray(new T[length])
 	{
+		for (unsigned int i = 0; i < length; i++)
+		{
+			mArray[i] = NULL;
+		}
 	}
 
 	template<typename T>
 	Storage<T>::Storage(unsigned int length, const T& initialValue)
+		: mLength(length)
+		, mArray(new T[length])
 	{
+		for (unsigned int i = 0; i < length; i++)
+		{
+			mArray[i] = initialValue;
+		}
+	}
+
+	template<typename T>
+	inline lab11::Storage<T>::Storage(Storage&& other)
+		: mArray(move(other.mArray))
+		, mLength(other.mLength)
+	{
+		other.mArray = nullptr;
+		other.mLength = 0;
+	}
+
+	template<typename T>
+	Storage<T> inline lab11::Storage<T>::operator=(Storage&& other)
+	{
+		if (this != &other)
+		{
+			mArray = nullptr;
+
+			mArray = other.mArray;
+			mLength = other.mLength;
+
+			other.mArray = nullptr;
+			other.mLength = 0;
+		}
+		return *this;
 	}
 
 	template<typename T>
 	bool Storage<T>::Update(unsigned int index, const T& data)
 	{
+		if (index < mLength)
+		{
+			mArray[index] = data;
+		}
 		return false;
 	}
 
 	template<typename T>
 	const std::unique_ptr<T[]>& Storage<T>::GetData() const
 	{
-		std::unique_ptr<T[]> temp = std::make_unique<T[]>(1);
-		return std::move(temp);
+		return mArray;
 	}
 
 	template<typename T>
 	unsigned int Storage<T>::GetSize() const
 	{
-		return 0;
+		return mLength;
 	}
 }
